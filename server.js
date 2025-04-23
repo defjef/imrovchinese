1c8263e44383042957ba756ad710ec4d268dbc96
Def Jef <jeftunes@gmail.com>
2025-04-22 21:12:28 -0400

    Simplified server to minimal configuration

--- server.js
+++ server.js
@@ -1,30 +1,11 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 
-// Basic CORS headers for the widget
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
-
-// Serve static files from the current directory
+// Serve static files
 app.use(express.static(__dirname));
 
-// Serve index.html for all routes (SPA support)
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-// Error handling
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-});
-
+// Start server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-    console.log(`Current directory: ${__dirname}`);
+    console.log('Server running on port', port);
 });
